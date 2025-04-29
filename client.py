87fff410ae4cad941ccdae87f511072f3fc0e590
shuheikomatsuki <shuheijy@gmail.com>
2025-04-29 13:38:02 +0900

    client側が2人の場合でもチャットできるようになった。TODO: 大人数でのチャット・リレーシステムから削除の実装

--- client.py
+++ client.py
@@ -7,24 +7,36 @@ def protocol_header(username_length):
 
 def send_message(sock, server_address, server_port, username):
     while True:
-        input_message = input(f"{username}: ")
-        if input_message.lower() == "exit":
-            break
-        message = protocol_header(len(username)) + (username + input_message).encode("utf-8")
-        sock.sendto(message, (server_address, server_port))
-        print(f"--- Sent: {input_message}")
+        try:
+            input_message = input(f"{username}: ")
+            if input_message.lower() == "exit":
+                break
+            message = protocol_header(len(username)) + (username + input_message).encode("utf-8")
+            sock.sendto(message, (server_address, server_port))
+            print(f"--- Sent: {input_message}")
+
+        except Exception as e:
+            print(f"Error sending message: {e}")
+            sock.close()
+            exit(1)
 
     sock.close()
     sys.exit(0)
 
 def receive_message(sock):
     while True:
-        data, _ = sock.recvfrom(4096)
-        username_length = int.from_bytes(data[:1], "big")
-        username = data[1:username_length + 1].decode("utf-8")
-        message = data[username_length + 1:].decode("utf-8")
-        print(f"{username}: {message}")
-        print(f"Received: {username}: {message}")
+        try:
+            data, _ = sock.recvfrom(4096)
+            username_length = int.from_bytes(data[:1], "big")
+            username = data[1:username_length + 1].decode("utf-8")
+            message = data[username_length + 1:].decode("utf-8")
+            print(f"{username}: {message}")
+            print(f"Received: {username}: {message}")
+
+        except Exception as e:
+            print(f"Error receiving message: {e}")
+            sock.close()
+            exit(1)
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,26 +45,28 @@ def main():
     server_port = 9001
     
     address = ""
-    port = 9050
+    port = 0
 
     sock.bind((address, port))
 
-    username = input("Type in your user name: ")
-    message = protocol_header(len(username)) + username.encode("utf-8")
-    sock.sendto(message, (server_address, server_port))
-    print(f"{username} has connected to the server.")
+    try:
+        username = input("Type in your user name: ")
+        message = protocol_header(len(username)) + username.encode("utf-8")
+        sock.sendto(message, (server_address, server_port))
+        print(f"{username} has connected to the server.")
 
-    send_thread = threading.Thread(target=send_message, args=(sock, server_address, server_port, username))
-    receive_thread = threading.Thread(target=receive_message, args=(sock,))
-    send_thread.start()
-    receive_thread.start()
-    send_thread.join()
-    receive_thread.join()
+        send_thread = threading.Thread(target=send_message, args=(sock, server_address, server_port, username))
+        receive_thread = threading.Thread(target=receive_message, args=(sock,))
+        send_thread.start()
+        receive_thread.start()
+        send_thread.join()
+        receive_thread.join()
+
+    except Exception as e:
+        print("\nExiting...")
+        sock.close()
+        exit(1)
 
 
 if __name__ == "__main__":
     main()
-    
-    
-
-
